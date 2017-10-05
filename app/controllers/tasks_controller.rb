3405685964184f0b7c93ce79702daa0153c6cd22
Yutaro Mizuno <yutaro.mizuno@gmail.com>
2017-10-05 14:04:55 +0000

    finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,54 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:edit,:destroy,:show]
 
+  def index
+   @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+  end
+  
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = 'タスクを追加しました。'
       redirect_to root_url
     else
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
-      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      flash.now[:danger] = 'タスクの追加に失敗しました。'
       render 'toppages/index'
     end
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def destroy
     @task.destroy
-    flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+    flash[:success] = 'タスクを削除しました。'
+    redirect_to @task
   end
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'task は更新されませんでした'
+      render :edit
+    end
+  end
+
 
   private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
   end
 
   def correct_user
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks
 end
